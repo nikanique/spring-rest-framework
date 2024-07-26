adbf8cbe071d67286539b857dc55b1674f3b4999
birddevelper <mostafa_shaeri_tj@yahoo.com>
2024-07-26 10:01:14 +0300

    feat: initial setup

---
+++ src/main/java/io/github/nikanique/springrestframework/SpringRestFramework.java
@@ -0,0 +1,10 @@
+package io.github.nikanique.springrestframework;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = {"io.github.nikanique.springrestframework"})
+@Configuration
+public class SpringRestFramework {
+
+}
