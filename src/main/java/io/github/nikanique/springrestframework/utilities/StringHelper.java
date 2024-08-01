7aec7528724915ab1641955e76cc5f12d871a667
birddevelper <mostafa_shaeri_tj@yahoo.com>
2024-08-01 16:53:25 +0300

    refactor: move capitalize method to StringHelper

--- src/main/java/io/github/nikanique/springrestframework/utilities/StringHelper.java
+++ src/main/java/io/github/nikanique/springrestframework/utilities/StringHelper.java
@@ -17,5 +17,12 @@ public class StringHelper {
         }
         return count;
     }
+    
+    public static String capitalize(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+    }
 
 }
