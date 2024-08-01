b0114cc4e7a6530d2ec516214b9c502ca7f35699
birddevelper <mostafa_shaeri_tj@yahoo.com>
2024-08-01 17:08:15 +0300

    refactor: move generateHashCode method to StringHelper

--- src/main/java/io/github/nikanique/springrestframework/utilities/StringHelper.java
+++ src/main/java/io/github/nikanique/springrestframework/utilities/StringHelper.java
@@ -17,12 +17,19 @@ public class StringHelper {
         }
         return count;
     }
-    
+
     public static String capitalize(String str) {
         if (str == null || str.isEmpty()) {
             return str;
         }
         return Character.toUpperCase(str.charAt(0)) + str.substring(1);
     }
+
+
+    public static String generateHashCode(String string) {
+        // Simple hash of the query string for caching purposes
+        return Integer.toHexString(string.hashCode());
+    }
+
 
 }
