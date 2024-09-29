1eced30b763c2671612fdb2d356b705a1f4be5f3
birddevelper <mostafa_shaeri_tj@yahoo.com>
2024-09-30 01:47:21 +0300

    refactor: add ICreateController interface

--- src/main/java/io/github/nikanique/springrestframework/web/controllers/CreateController.java
+++ src/main/java/io/github/nikanique/springrestframework/web/controllers/CreateController.java
@@ -5,48 +5,32 @@ import io.github.nikanique.springrestframework.orm.EntityBuilder;
 import io.github.nikanique.springrestframework.serializer.SerializerConfig;
 import io.github.nikanique.springrestframework.services.CommandService;
 import io.github.nikanique.springrestframework.services.QueryService;
-import io.github.nikanique.springrestframework.swagger.CreateSchemaGenerator;
 import io.swagger.v3.oas.models.Operation;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.method.HandlerMethod;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 
 @Getter
 public abstract class CreateController<Model, ID, ModelRepository extends JpaRepository<Model, ID>>
         extends BaseGenericController<Model, ID, ModelRepository>
-        implements CreateSchemaGenerator {
-
+        implements ICreateController<Model, ID> {
 
     final private SerializerConfig createResponseSerializerConfig;
     private EntityBuilder<Model> entityHelper;
     private CommandService<Model, ID> commandService;
     private QueryService<Model> queryService;
 
-
     @Autowired
     public CreateController(@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") ModelRepository repository) {
         super(repository);
         this.createResponseSerializerConfig = configCreateSerializerFields();
-    }
-
-    private static String getRequestBody(HttpServletRequest request) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        try (BufferedReader reader = request.getReader()) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-        }
-        return sb.toString();
     }
 
     @PostConstruct
@@ -55,34 +39,22 @@ public abstract class CreateController<Model, ID, ModelRepository extends JpaRep
         this.entityHelper = EntityBuilder.getInstance(this.getModel(), this.context);
     }
 
-    protected Class<?> getCreateRequestBodyDTO() {
+    public Class<?> getCreateRequestBodyDTO() {
         return getDTO();
     }
 
-    protected Class<?> getCreateResponseBodyDTO() {
+    public Class<?> getCreateResponseBodyDTO() {
         return getDTO();
-    }
-
-
-    public SerializerConfig configCreateSerializerFields() {
-        return SerializerConfig.fromDTO(getCreateResponseBodyDTO());
     }
 
     @PostMapping("/")
-    public ResponseEntity<ObjectNode> create(HttpServletRequest request) throws IOException {
-        String requestBody = getRequestBody(request);
-        Object dto = this.serializer.deserialize(requestBody, getCreateRequestBodyDTO());
-        Model entity = this.getEntityHelper().fromDto(dto, this.getCreateRequestBodyDTO());
-        entity = commandService.create(entity);
-        return ResponseEntity.status(HttpStatus.CREATED).body(
-                serializer.serialize(entity, getCreateResponseSerializerConfig())
-        );
+    public ResponseEntity<ObjectNode> post(HttpServletRequest request) throws IOException {
+        return this.create(this, request);
     }
 
-
     public void customizeOperationForController(Operation operation, HandlerMethod handlerMethod) {
-        if (handlerMethod.getMethod().getName().equals("create")) {
-            generateCreateSchema(operation, getCreateRequestBodyDTO(), getCreateResponseBodyDTO());
+        if (handlerMethod.getMethod().getName().equals("post")) {
+            this.generateCreateSchema(operation, getCreateRequestBodyDTO(), getCreateResponseBodyDTO());
         }
     }
 
---
+++ src/main/java/io/github/nikanique/springrestframework/web/controllers/ICreateController.java
@@ -0,0 +1,70 @@
+package io.github.nikanique.springrestframework.web.controllers;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.github.nikanique.springrestframework.common.EndpointType;
+import io.github.nikanique.springrestframework.orm.EntityBuilder;
+import io.github.nikanique.springrestframework.serializer.SerializerConfig;
+import io.github.nikanique.springrestframework.services.CommandService;
+import io.github.nikanique.springrestframework.swagger.SwaggerSchemaGenerator;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public interface ICreateController<Model, ID> {
+    SerializerConfig getCreateResponseSerializerConfig();
+
+    Class<?> getCreateRequestBodyDTO();
+
+    Class<?> getCreateResponseBodyDTO();
+
+    EntityBuilder<Model> getEntityHelper();
+
+    CommandService<Model, ID> getCommandService();
+
+    default SerializerConfig configCreateSerializerFields() {
+        return SerializerConfig.fromDTO(getCreateResponseBodyDTO());
+    }
+
+    default String getRequestBody(HttpServletRequest request) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = request.getReader()) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        }
+        return sb.toString();
+    }
+
+    default ResponseEntity<ObjectNode> create(BaseGenericController controller, HttpServletRequest request) throws IOException {
+        String requestBody = getRequestBody(request);
+        Object dto = controller.getSerializer().deserialize(requestBody, getCreateRequestBodyDTO());
+        Model entity = this.getEntityHelper().fromDto(dto, this.getCreateRequestBodyDTO());
+        entity = getCommandService().create(entity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                controller.getSerializer().serialize(entity, getCreateResponseSerializerConfig())
+        );
+    }
+
+    default void generateCreateSchema(Operation operation, Class<?> createRequestBodyDTO, Class<?> createResponseDTO) {
+        Schema<?> requestBodySchema = SwaggerSchemaGenerator.generateSchema(createRequestBodyDTO, EndpointType.WRITE);
+        Content content = new Content().addMediaType("application/json", new MediaType().schema(requestBodySchema));
+
+        operation.requestBody(new io.swagger.v3.oas.models.parameters.RequestBody().content(content));
+
+        // Generate Response schema
+        Schema<?> responseSchema = SwaggerSchemaGenerator.generateSchema(createResponseDTO, EndpointType.READ);
+        ApiResponse response = new ApiResponse().content(new Content().addMediaType("application/json",
+                new MediaType().schema(responseSchema)));
+        operation.responses(new io.swagger.v3.oas.models.responses.ApiResponses()
+                .addApiResponse("201", response));
+    }
+}
