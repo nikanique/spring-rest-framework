2cf3fad6eebfcb37dcd5d15d58b01e02ccf23924
birddevelper <mostafa_shaeri_tj@yahoo.com>
2024-12-24 22:44:29 +0200

    fix: Handle UnauthorizedException

--- src/main/java/io/github/nikanique/springrestframework/advices/GlobalExceptionHandler.java
+++ src/main/java/io/github/nikanique/springrestframework/advices/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package io.github.nikanique.springrestframework.advices;
 
 import io.github.nikanique.springrestframework.exceptions.BadRequestException;
 import io.github.nikanique.springrestframework.exceptions.BaseException;
+import io.github.nikanique.springrestframework.exceptions.UnauthorizedException;
 import io.github.nikanique.springrestframework.exceptions.ValidationException;
 import io.github.nikanique.springrestframework.web.responses.ErrorResponse;
+import io.github.nikanique.springrestframework.web.responses.ValidationErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,10 +17,15 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({ValidationException.class, BadRequestException.class})
     public ResponseEntity<ErrorResponse> handleValidationException(BaseException ex) {
-        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), ex.getErrors());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        ValidationErrorResponse validationErrorResponse = new ValidationErrorResponse(ex.getMessage(), ex.getErrors());
+        return new ResponseEntity<>(validationErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler({UnauthorizedException.class})
+    public ResponseEntity<ErrorResponse> handleUnauthorizedException(BaseException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+    }
 
 }
 
